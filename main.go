ef703b5dc3bc9793c5d9e7b7c0da1f1a711a36d8
aelhadda <eaub@proton.me>
2024-04-20 20:23:30 +0100

    not yet

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -13,111 +14,43 @@ import (
 //Declaring variables
 
 var (
+	text                   []string
+	base                   int
 	inputFileName          string
 	outputFileName         string
-	input                  []byte
+	input                  string
 	err                    error
 	lowRegex               = regexp.MustCompile(`(\(low, (\d+)\))`)
 	upRegex                = regexp.MustCompile(`(\(up, (\d+)\))`)
 	capRegex               = regexp.MustCompile(`(\(cap, (\d+)\))`)
 	onePunctuationRegex    = regexp.MustCompile(`(\s*([.,!?:;]+)+\s*)`)
+	lastPunctuationRegex   = regexp.MustCompile(`(\s*([.,!?:;]+)+\s*$)`)
 	groupPunctuationsRegex = regexp.MustCompile(`([.,!?:;\s]+[.,!?:;])`)
-	vowelsRegex            = regexp.MustCompile(`((\s+[aA])\s([aeiouh]))`)
+	vowelsRegex            = regexp.MustCompile(`((\s*[aA])\s([aeiouh]))`)
 	quotesRegex            = regexp.MustCompile(`('\s*([-.,!?:;]*\w+(?:[-.,!?:;\s]+\w+)+[-.,!?:;]*)\s*')`)
 	delimiter              = 1
 	match                  []string
 )
 
-func main() {
-	//Check for valid arguments
-	if len(os.Args) != 3 {
-		fmt.Fprintln(os.Stderr, "Invalid arguments Usage : go run main.go <input.txt> <output.txt>")
-		return
-	}
-	inputFileName = os.Args[1]
-	outputFileName = os.Args[2]
-	//Read file content
-	input, err = ioutil.ReadFile(inputFileName)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while reading content in the file\nError: %v\n", err)
-		return
-	}
-	text := splitString(input)
-	//extracting regex match
-	for i := 0; i <= len(text)-1; i++ {
-		if len(text) <= 1 {
-			break
-		}
-		if text[i] == "(low)" {
-			text[i-1] = strings.ToLower(text[i-1])
-			text = deleteActions(text, i)
-		} else if lowRegex.FindStringSubmatch(text[i]) != nil {
-			match = lowRegex.FindStringSubmatch(text[i])
-			delimiter, _ = strconv.Atoi(match[2])
-			if i-delimiter < 0 {
-				fmt.Fprintf(os.Stderr, "The amount of words that you want to lowercase is not enough")
-				continue
+func readFile(inputFileName string) (string, error) {
+	content, err := ioutil.ReadFile(inputFileName)
+	return string(content), err
+}
+
+func splitString(input string) []string {
+	split := strings.Split(input, " ")
+	//Check if the next element of the slice is the second part of the action, Example : ["(cap," "333)"] and concatenate them
+	for index, word := range split {
+		if word == "(low," || word == "(up," || word == "(cap," {
+			if index != len(split)-1 && isTheSecondPart(split[index+1]) {
+				split[index] = split[index] + " " + split[index+1]
+				split = append(split[:index+1], split[index+2:]...)
 			}
-			for j := i - 1; j+delimiter >= i; j-- {
-				text[j] = strings.ToLower(text[j])
-			}
-			text = deleteActions(text, i)
-			delimiter = 1
-		} else if text[i] == "(up)" {
-			text[i-1] = strings.ToUpper(text[i-1])
-			text = deleteActions(text, i)
-		} else if upRegex.FindStringSubmatch(text[i]) != nil {
-			match = upRegex.FindStringSubmatch(text[i])
-			delimiter, _ = strconv.Atoi(match[2])
-			if i-delimiter < 0 {
-				fmt.Fprintf(os.Stderr, "The amount of words that you want to uppercase is not enough")
-				continue
-			}
-			for j := i - 1; j+delimiter >= i; j-- {
-				text[j] = strings.ToUpper(text[j])
-			}
-			text = deleteActions(text, i)
-			delimiter = 1
-		} else if text[i] == "(cap)" {
-			text[i-1] = capitalize(text[i-1])
-			text = deleteActions(text, i)
-		} else if capRegex.FindStringSubmatch(text[i]) != nil {
-			match = capRegex.FindStringSubmatch(text[i])
-			delimiter, _ = strconv.Atoi(match[2])
-			if i-delimiter < 0 {
-				fmt.Fprintf(os.Stderr, "The amount of words that you want to capitalize is not enough")
-				continue
-			}
-			for j := i - 1; j+delimiter >= i; j-- {
-				text[j] = capitalize(text[j])
-			}
-			delimiter = 1
-			text = deleteActions(text, i)
-		} else if text[i] == "(hex)" {
-			coverted, err := strconv.ParseInt(text[i-1], 16, 0)
-			if err != nil {
-				panic(err)
-			}
-			text[i-1] = strconv.Itoa(int(coverted))
-			text = deleteActions(text, i)
-		} else if text[i] == "(bin)" {
-			coverted, _ := strconv.ParseInt(text[i-1], 2, 0)
-			text[i-1] = strconv.Itoa(int(coverted))
-			text = deleteActions(text, i)
+		} else {
+			continue
 		}
 	}
-	// join slice
-	strText := strings.Join(text, " ")
-	// Handle space before and after punctuations
-	strText = onePunctFunc(strText)
-	//Handle successive punctuations
-	strText = groupPunctFunc(strText)
-	// add n to a if a *vowel*
-	strText = vowelFix(strText)
-	// single quotes fix
-	strText = quotesRegex.ReplaceAllString(strText, "'$2'")
-	writeToOutput(strText, outputFileName)
-	fmt.Println(strText)
+	return split
 }
 
 func isTheSecondPart(s string) bool {
@@ -139,33 +72,21 @@ func isTheSecondPart(s string) bool {
 	return false
 }
 
-func splitString(input []byte) []string {
-	split := strings.Split(string(input), " ")
-	for index, word := range split {
-		//Check if the next element of the slice is the second part of the action, Example : ["(cap," "333)"] and concatenate them
-		if word == "(low," || word == "(up," || word == "(cap," {
-			if index != len(split)-1 && isTheSecondPart(split[index+1]) {
-				split[index] = split[index] + " " + split[index+1]
-				split = append(split[:index+1], split[index+2:]...)
-			}
-		} else {
-			continue
-		}
-	}
-	return split
-}
-
-func capitalize(s string) string {
+func capitalize(s string) (string, error) {
+	var err error
 	rs := []rune(s)
 
 	if len(rs) == 0 {
-		return ""
+		return "", err
+	}
+	if !(rs[0] >= 'a' && rs[0] <= 'z' || rs[0] >= 'A' && rs[0] <= 'Z') {
+		err = errors.New("capitalize : not valid syntax")
 	}
 	rs[0] = unicode.ToUpper(rs[0])
 	for i := 1; i < len(rs); i++ {
 		rs[i] = unicode.ToLower(rs[i])
 	}
-	return string(rs)
+	return string(rs), err
 }
 
 func deleteActions(text []string, toDelete int) []string {
@@ -175,6 +96,7 @@ func deleteActions(text []string, toDelete int) []string {
 
 func onePunctFunc(text string) string {
 	text = onePunctuationRegex.ReplaceAllString(text, "$2 ")
+	text = lastPunctuationRegex.ReplaceAllString(text, "$2")
 	return text
 }
 
@@ -198,4 +120,160 @@ func writeToOutput(toWrite string, outputFileName string) {
 		fmt.Fprintf(os.Stderr, "Error while writing content in the file\nError: %v\n", err)
 		return
 	}
+}
+
+func main() {
+	//Check for valid arguments
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "Invalid arguments Usage : go run main.go <input.txt> <output.txt>")
+		return
+	}
+	inputFileName = os.Args[1]
+	outputFileName = os.Args[2]
+	//Read file content
+	input, err = readFile(inputFileName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while reading content in the file\nError: %v\n", err)
+		return
+	}
+	input = onePunctFunc(input)
+	input = groupPunctFunc(input)
+	//split string
+	text = splitString(input)
+	//matching user prompts
+	for i := 0; i < len(text); i++ {
+		if len(text) <= 1 {
+			break
+		}
+		if text[i] == "(low)" {
+			k := i
+			if k-1 < 0 {
+				fmt.Fprintf(os.Stderr, "The amount of words that you want to lowercase is not enough")
+				continue
+			}
+			for text[k-1] == "" {
+				k -= 1
+			}
+			text[k-1] = strings.ToLower(text[k-1])
+			text = deleteActions(text, i)
+		} else if lowRegex.FindStringSubmatch(text[i]) != nil {
+			match = lowRegex.FindStringSubmatch(text[i])
+			delimiter, _ = strconv.Atoi(match[2])
+			k := i
+			for text[k-1] == "" {
+				k -= 1
+			}
+			if k-delimiter < 0 {
+				fmt.Fprintf(os.Stderr, "The amount of words that you want to lowercase is not enough")
+				continue
+			}
+			for j := k - 1; j+delimiter >= k; j-- {
+				text[j] = strings.ToLower(text[j])
+			}
+			text = deleteActions(text, i)
+			delimiter = 1
+		} else if text[i] == "(up)" {
+			k := i
+			if k-1 < 0 {
+				fmt.Fprintf(os.Stderr, "The amount of words that you want to uppercase is not enough")
+				continue
+			}
+			for text[k-1] == "" {
+				k -= 1
+			}
+			text[k-1] = strings.ToUpper(text[k-1])
+			text = deleteActions(text, i)
+		} else if upRegex.FindStringSubmatch(text[i]) != nil {
+			match = upRegex.FindStringSubmatch(text[i])
+			delimiter, _ = strconv.Atoi(match[2])
+			k := i
+			for text[k-1] == "" {
+				k -= 1
+			}
+			if k-delimiter < 0 {
+				fmt.Fprintf(os.Stderr, "The amount of words that you want to uppercase is not enough")
+				continue
+			}
+			for j := k - 1; j+delimiter >= k; j-- {
+				text[j] = strings.ToUpper(text[j])
+			}
+			text = deleteActions(text, i)
+			delimiter = 1
+		} else if text[i] == "(cap)" {
+			k := i
+			if k-1 < 0 {
+				fmt.Fprintf(os.Stderr, "The amount of words that you want to capitalize is not enough")
+				continue
+			}
+			for text[k-1] == "" {
+				k -= 1
+			}
+			text[k-1], err = capitalize(text[k-1])
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			text = deleteActions(text, i)
+		} else if capRegex.FindStringSubmatch(text[i]) != nil {
+			match = capRegex.FindStringSubmatch(text[i])
+			delimiter, _ = strconv.Atoi(match[2])
+			k := i
+			for text[k-1] == "" {
+				k -= 1
+			}
+			if k-delimiter < 0 {
+				fmt.Fprintf(os.Stderr, "The amount of words that you want to capitalize is not enough")
+				continue
+			}
+			for j := k - 1; j+delimiter >= k; j-- {
+				text[j], err = capitalize(text[j])
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+					break
+				}
+			}
+			delimiter = 1
+			text = deleteActions(text, i)
+		} else if text[i] == "(hex)" {
+			k := i
+			if k-1 < 0 {
+				fmt.Fprintf(os.Stderr, "The amount of words that you want to convert to decimal is not enough")
+				continue
+			}
+			for text[k-1] == "" {
+				k -= 1
+			}
+
+			coverted, err := strconv.ParseInt(text[k-1], 16, 0)
+			if err != nil {
+				panic(err)
+			}
+			text[k-1] = strconv.Itoa(int(coverted))
+			text = deleteActions(text, i)
+		} else if text[i] == "(bin)" {
+			k := i
+			if k-1 < 0 {
+				fmt.Fprintf(os.Stderr, "The amount of words that you want to convert to decimal is not enough")
+				continue
+			}
+			for text[k-1] == "" {
+				k -= 1
+			}
+
+			coverted, _ := strconv.ParseInt(text[k-1], 2, 0)
+			text[k-1] = strconv.Itoa(int(coverted))
+			text = deleteActions(text, i)
+		}
+	}
+	// join slice
+	strText := strings.Join(text, " ")
+	// Handle space before and after punctuations
+	strText = onePunctFunc(strText)
+	//Handle successive punctuations
+	strText = groupPunctFunc(strText)
+	// add n to a if a *vowel*
+	strText = vowelFix(strText)
+	// single quotes fix
+	strText = quotesRegex.ReplaceAllString(strText, "'$2'")
+	writeToOutput(strText, outputFileName)
 }
